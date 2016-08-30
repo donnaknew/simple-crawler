6d8fcfbaeb267c0af078780bb451a93a573aaf51
Jeesang Kim <arenalkim@gmail.com>
2016-08-30 14:30:27 +0900

    add select p > span function

--- webCrawler.py
+++ webCrawler.py
@@ -8,13 +8,23 @@ def spider(max_pages):
         source_code = requests.get(url)
         plain_text = source_code.text
         soup = BeautifulSoup(plain_text, 'lxml')
+
         for title_list in soup.find_all(['h3', 'class']):
             href = url
             title = title_list.text
             print(href)
             print(title)
+            get_single_article(href)
 
         #print(soup)
         page += 1
+
+def get_single_article(item_url):
+    source_code = requests.get(item_url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text, 'lxml')
+
+    for contents in soup.select('p > span'):
+        print(contents.text)
 
 spider(10)
